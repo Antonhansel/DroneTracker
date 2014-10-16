a3599200614d3fc62e0e8f86c738be2707f73383
antonhansel <antoninribeaud@gmail.com>
2014-10-16 10:39:12 +0200

    Wrote a little api that serves drone data as JSON on port 3000 at /api/navdata

--- api/navData.js
+++ api/navData.js
@@ -1,28 +1,14 @@
-module.exports = function(droneSocket)
+module.exports = function(droneSocket, router)
 {
-	var express = require("express");
-	var api = express();
-	var router = express.router();
-	var bodyParser = require("body-parser");
 	var lastData = "none";
-
-	api.use(bodyParser.json());
-	router.use(function(req, res, next))
-	{
-		console.log("Request: " + req);
-		next();
-	}
-	api.use("/api", router);
-	api.listen(3000);
-
 
 	droneSocket.on('navdata', function(navdata)
 	{
-		lastData = data;
+		lastData = navdata;
 	});
 
 	router.get("/navdata", function(req, res)
 	{
 		res.json(lastData);
-	}
+	});
 }
--- droneTracker.js
+++ droneTracker.js
@@ -1,3 +1,4 @@
+//drone opencv and cmdline stuff
 var cv = require('opencv');
 var drone = require('ar-drone');
 var http = require('http');
@@ -7,8 +8,25 @@ var droneSocket = drone.createClient();
 var cmdLine = require("./modules/commandLine")(droneSocket, s);
 var faceDetect = require("./modules/faceDetect");
 
+//Api stuff
+var express = require("express");
+var api = express();
+var router = express.Router();
+var bodyParser = require("body-parser");
+
+api.use(bodyParser.json());
+router.use(function(req, res, next)
+{
+	console.log("Request: " + req);
+	next();
+});
+api.use("/api", router);
+api.listen(3000);
+
+var navData = require("./api/navData")(droneSocket, router);
+
+//image handling stuff
 var lastFrame;
-var lastData = "none";
 
 s.on('data', _.throttle(function(matrix)
 {
@@ -20,18 +38,14 @@ s.on('data', function(data)
 	lastFrame = data;
 })
 
-droneSocket.on('navdata', function(navdata)
-	{
-		lastData = data;
-	});
-
 var server = http.createServer(function(req, res)
 {
-    res.writeHead(200, {'Content-Type': 'text'});
-    res.write("local server up.");
-    res.end();
+	res.writeHead(200, {'Content-Type': 'image/png'});
+	res.write("local server up.");
+	res.end();
 });
 
 server.listen(8080);
 
+//piping pngstream to opencv handler
 droneSocket.getPngStream().pipe(s);
