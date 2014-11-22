7f1d08b0a92af1907e051bfa87f7b99fd9ad2b81
antonhansel <antoninribeaud@gmail.com>
2014-11-22 13:50:56 +0100

    Refactoring

--- droneTracker.js
+++ droneTracker.js
@@ -15,38 +15,30 @@ var router = express.Router();
 var bodyParser = require("body-parser");
 
 api.use(bodyParser.json());
-router.use(function(req, res, next)
-{
+router.use(function(req, res, next){
 	console.log("Request: " + req);
 	next();
 });
+
 api.use("/api", router);
 api.listen(3000);
 
 var navData = require("./api/navData")(droneSocket, router);
 
 //image handling stuff
-var lastFrame;
 
-s.on('data', _.throttle(function(matrix)
-{
-	faceDetect.matrixHandler(matrix, cv, droneSocket);
-}, 100))
-
-s.on('data', function(data)
-{
-	lastFrame = data;
+s.on('data', function(matrix){
+	//faceDetect.matrixHandler(matrix, cv, droneSocket);
+	console.log(Date());
 })
 
-var server = http.createServer(function(req, res)
-{
+var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'image/png'});
 	res.write("local server up.");
 	res.end();
 });
 
 server.listen(8080);
-
+console.log("server is listening on 8080");
 //piping pngstream to opencv handler
 droneSocket.getPngStream().pipe(s);
-var stream = droneSocket.getPngStream();
--- modules/commandLine.js
+++ modules/commandLine.js
@@ -1,42 +1,30 @@
-/* 
-* @Author: antonhansel
-* @Date:   2014-10-15 14:48:01
-* @Last Modified by:   antonhansel
-* @Last Modified time: 2014-10-15 16:36:05
-*/
+var readline = require('readline');
 
 module.exports = function(droneSocket, s)
 {
-	var readline = require('readline');
-
 	var rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout
 	});
 
 	rl.on('line', function (cmd) {
-		if (cmd == "on")
-		{
+		if (cmd == "on"){
 			droneSocket.stop();
 			droneSocket.takeoff();
 		}
-		else if (cmd == "off")
-		{
+		else if (cmd == "off"){
 			droneSocket.stop();
 			droneSocket.land();
 		}
-		if (cmd == "stop")
-		{
+		if (cmd == "stop"){
 			console.log("Stopping all drone actions...");
 			droneSocket.stop();
 		}
-		if (cmd == "up")
-		{
+		if (cmd == "up"){
 			console.log("Going up...");
 			droneSocket.up(0.2);
 		}
-		if (cmd == "start")
-		{
+		if (cmd == "start"){
 			droneSocket.getPngStream().pipe(s);
 		}
 	});
